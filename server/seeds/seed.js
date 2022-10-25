0bb77671d4e10737bb6416d56fcac92370d37846
Camille Yong <camillionaire@Camilles-MacBook-Pro.local>
2022-10-25 14:01:28 -0700

    changed tyeDefs, added seeds data files, changed resolver data, and added user seeds.js

--- server/seeds/seed.js
+++ server/seeds/seed.js
@@ -1,13 +1,22 @@
 const db = require('../config/connection');
-const { Planets } = require('../models');
+const { Planets, User} = require('../models');
 
-// ! COMMENTED OUT FOR NOW
-// const planetsData = require('./planetsData.json');
+const planetsData = require('./planetsData.json');
+const usersData = require('./usersData.json');
 
 db.once('open', async () => {
   await Planets.deleteMany({});
 
   const planets = await Planets.insertMany(planetsData);
+
+  console.log('Planets seeded!');
+  process.exit(0);
+});
+
+db.once('open', async () => {
+  await User.deleteMany({});
+
+  const planets = await User.insertMany(usersData);
 
   console.log('Planets seeded!');
   process.exit(0);
