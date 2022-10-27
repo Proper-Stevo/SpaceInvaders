a8a162541d473d5a96f13c0bc9e9da19cf31dc25
Martha A <mladame43@gmail.com>
2022-10-27 11:58:25 -0700

    added mutations starter, added initial buttons for functions

--- client/src/pages/Planet/PlanetComment.js
+++ client/src/pages/Planet/PlanetComment.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import React, { useState } from 'react';
 import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
 
 
 export default function PlanetComment() {
@@ -8,24 +9,30 @@ export default function PlanetComment() {
     // define userComment through query
     // mutations to update comment
 
-    // use state
+    // use state  (change active btwn cards&textarea)
+    // use mutations (add/update/delete comment)
+    // import { useMutation } from '@apollo/client';
+    // import { SAVE_BOOK } from '../utils/mutations';
 
 
     return !userComment ? (
         <div className='userCommentCard'>
-        <Card>
-        <Card.Body>User notes go here</Card.Body>
-        </Card>
+            <Card>
+                <Card.Body>User notes go here</Card.Body>
+            </Card>
+            <Button variant="outline-primary">Update Notes</Button>{' '}
+            <Button variant="outline-primary">Delete Notes</Button>{' '}
         </div>
     ) : (
         <div className='userInputComment'>
-        <FloatingLabel controlId="floatingTextarea2" label="Comments">
-        <Form.Control
-        as="textarea"
-        placeholder="Leave a comment here"
-        style={{ height: '100px' }}
-        />
-        </FloatingLabel>
+            <FloatingLabel controlId="floatingTextarea2" label="Comments">
+                <Form.Control
+                    as="textarea"
+                    placeholder="Leave a comment here"
+                    style={{ height: '100px' }}
+                />
+            </FloatingLabel>
+            <Button variant="outline-primary">Save Notes</Button>{' '}
         </div>
     )
 };
