b98e19fdc79e6148e01461121755598e555f7721
Martha A <mladame43@gmail.com>
2022-10-27 06:54:34 -0700

    changed card subtitles in planetfacts, imported/commented out imported mutations, in progress

--- client/src/pages/Planet/PlanetFacts.js
+++ client/src/pages/Planet/PlanetFacts.js
@@ -1,56 +1,58 @@
 import React from 'react';
 import Card from 'react-bootstrap/Card';
 
-import PlanetFacts from './PlanetFacts';
+// import { useMutation } from '@apollo/client';
+// MUTATION/QUERY????
+// import { PLANET_FACTS } from '../utils/mutations';
 
 export default function Planet() {
     return (
         <div className='planetFactsContainer'>
             <Card style={{ width: '18rem' }}>
                 <Card.Body>
-                    <Card.Subtitle className="mb-2 text-muted">Planet</Card.Subtitle>
+                    <Card.Subtitle className="mb-2 text-muted">Mass</Card.Subtitle>
                     <Card.Title>Jupiter</Card.Title>
                 </Card.Body>
             </Card>
             <Card style={{ width: '18rem' }}>
                 <Card.Body>
-                    <Card.Subtitle className="mb-2 text-muted">Planet</Card.Subtitle>
+                    <Card.Subtitle className="mb-2 text-muted">Gravity</Card.Subtitle>
                     <Card.Title>Jupiter</Card.Title>
                 </Card.Body>
             </Card>
             <Card style={{ width: '18rem' }}>
                 <Card.Body>
-                    <Card.Subtitle className="mb-2 text-muted">Planet</Card.Subtitle>
+                    <Card.Subtitle className="mb-2 text-muted">Density</Card.Subtitle>
                     <Card.Title>Jupiter</Card.Title>
                 </Card.Body>
             </Card>
             <Card style={{ width: '18rem' }}>
                 <Card.Body>
-                    <Card.Subtitle className="mb-2 text-muted">Planet</Card.Subtitle>
+                    <Card.Subtitle className="mb-2 text-muted">Average Temp.</Card.Subtitle>
                     <Card.Title>Jupiter</Card.Title>
                 </Card.Body>
             </Card>
             <Card style={{ width: '18rem' }}>
                 <Card.Body>
-                    <Card.Subtitle className="mb-2 text-muted">Planet</Card.Subtitle>
+                    <Card.Subtitle className="mb-2 text-muted">Sideral Orbit</Card.Subtitle>
                     <Card.Title>Jupiter</Card.Title>
                 </Card.Body>
             </Card>
             <Card style={{ width: '18rem' }}>
                 <Card.Body>
-                    <Card.Subtitle className="mb-2 text-muted">Planet</Card.Subtitle>
+                    <Card.Subtitle className="mb-2 text-muted">Sideral Rotation</Card.Subtitle>
                     <Card.Title>Jupiter</Card.Title>
                 </Card.Body>
             </Card>
             <Card style={{ width: '18rem' }}>
                 <Card.Body>
-                    <Card.Subtitle className="mb-2 text-muted">Planet</Card.Subtitle>
+                    <Card.Subtitle className="mb-2 text-muted">Distance from Sun</Card.Subtitle>
                     <Card.Title>Jupiter</Card.Title>
                 </Card.Body>
             </Card>
             <Card style={{ width: '18rem' }}>
                 <Card.Body>
-                    <Card.Subtitle className="mb-2 text-muted">Planet</Card.Subtitle>
+                    <Card.Subtitle className="mb-2 text-muted">Year Length</Card.Subtitle>
                     <Card.Title>Jupiter</Card.Title>
                 </Card.Body>
             </Card>
