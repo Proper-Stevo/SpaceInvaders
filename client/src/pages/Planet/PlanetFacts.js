dfc47c7ca01a8b88fc64006e60739695a1b7ec6d
Martha A <mladame43@gmail.com>
2022-10-27 06:41:12 -0700

    added initial cards for planetFacts.js

---
+++ client/src/pages/Planet/PlanetFacts.js
@@ -0,0 +1,60 @@
+import React from 'react';
+import Card from 'react-bootstrap/Card';
+
+import PlanetFacts from './PlanetFacts';
+
+export default function Planet() {
+    return (
+        <div className='planetFactsContainer'>
+            <Card style={{ width: '18rem' }}>
+                <Card.Body>
+                    <Card.Subtitle className="mb-2 text-muted">Planet</Card.Subtitle>
+                    <Card.Title>Jupiter</Card.Title>
+                </Card.Body>
+            </Card>
+            <Card style={{ width: '18rem' }}>
+                <Card.Body>
+                    <Card.Subtitle className="mb-2 text-muted">Planet</Card.Subtitle>
+                    <Card.Title>Jupiter</Card.Title>
+                </Card.Body>
+            </Card>
+            <Card style={{ width: '18rem' }}>
+                <Card.Body>
+                    <Card.Subtitle className="mb-2 text-muted">Planet</Card.Subtitle>
+                    <Card.Title>Jupiter</Card.Title>
+                </Card.Body>
+            </Card>
+            <Card style={{ width: '18rem' }}>
+                <Card.Body>
+                    <Card.Subtitle className="mb-2 text-muted">Planet</Card.Subtitle>
+                    <Card.Title>Jupiter</Card.Title>
+                </Card.Body>
+            </Card>
+            <Card style={{ width: '18rem' }}>
+                <Card.Body>
+                    <Card.Subtitle className="mb-2 text-muted">Planet</Card.Subtitle>
+                    <Card.Title>Jupiter</Card.Title>
+                </Card.Body>
+            </Card>
+            <Card style={{ width: '18rem' }}>
+                <Card.Body>
+                    <Card.Subtitle className="mb-2 text-muted">Planet</Card.Subtitle>
+                    <Card.Title>Jupiter</Card.Title>
+                </Card.Body>
+            </Card>
+            <Card style={{ width: '18rem' }}>
+                <Card.Body>
+                    <Card.Subtitle className="mb-2 text-muted">Planet</Card.Subtitle>
+                    <Card.Title>Jupiter</Card.Title>
+                </Card.Body>
+            </Card>
+            <Card style={{ width: '18rem' }}>
+                <Card.Body>
+                    <Card.Subtitle className="mb-2 text-muted">Planet</Card.Subtitle>
+                    <Card.Title>Jupiter</Card.Title>
+                </Card.Body>
+            </Card>
+        </div>
+        
+    )
+};
