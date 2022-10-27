072db3b752ef5c365995fb904f52865547c7278c
Martha A <mladame43@gmail.com>
2022-10-27 11:40:54 -0700

    added initial planetcomment js, added if statment to show a card if usercomment, text area if no comment

--- client/src/pages/Planet/PlanetComment.js
+++ client/src/pages/Planet/PlanetComment.js
@@ -1,12 +1,31 @@
 import React from 'react';
+import React, { useState } from 'react';
 import Card from 'react-bootstrap/Card';
 
-export default function PlanetComment(){
+
+export default function PlanetComment() {
+
+    // define userComment through query
+    // mutations to update comment
+
+    // use state
 
 
-    return(
+    return !userComment ? (
+        <div className='userCommentCard'>
         <Card>
         <Card.Body>User notes go here</Card.Body>
         </Card>
+        </div>
+    ) : (
+        <div className='userInputComment'>
+        <FloatingLabel controlId="floatingTextarea2" label="Comments">
+        <Form.Control
+        as="textarea"
+        placeholder="Leave a comment here"
+        style={{ height: '100px' }}
+        />
+        </FloatingLabel>
+        </div>
     )
 };
