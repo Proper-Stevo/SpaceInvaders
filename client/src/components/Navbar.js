9b826318a45c293a669e203528be8cea5b67b50f
Martha A <mladame43@gmail.com>
2022-10-29 15:20:51 -0700

    added signup page initial code, in progress, adding 404 not found error page for users

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,109 +1,83 @@
 // show login/logout conditional
 // click on notes/comments in navbar, then see a list of comments associated with planets, takes you to planet page with comment
 
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
+import React, { useState } from 'react';
+import { Link, Route } from 'react-router-dom';
+import { Navbar, Nav, Container, Modal, Tab } from 'react-bootstrap';
+import SignUpForm from '../pages/Signup';
+import LoginForm from '../pages/Login';
 
-function BasicExample() {
+import Auth from '../utils/auth';
+
+export default function Navigation () {
+
+  const [showModal, setShowModal] = useState(false);
+
   return (
-    <Navbar bg="light" expand="lg">
-      <Container>
-        <Navbar.Brand href="#home">Orbital Odyssey</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            {/* needs auth */}
-            <Nav.Link href="#link">Planet Information</Nav.Link>
-            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-              {/* add conditional rendering */}
-              <NavDropdown.Item href="#action/3.1">Login</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Logout
-              </NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
-              </NavDropdown.Item>
-            </NavDropdown>
+    <>
+    <Navbar bg='dark' variant='dark' expand='lg'>
+      <Container fluid>
+        <Navbar.Brand as={Link} to='/'>
+          Orbital Odyssey
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls='navbar' />
+        <Navbar.Collapse id='navbar'>
+          <Nav className='ml-auto'>
+            {/* add 404 page */}
+            {/* <Route 
+              path='*' 
+              element={<NotFound>}
+            /> */}
+            {/* <Nav.Link as={Link} to='*'>
+              Planets
+            </Nav.Link> */}
+            {/* if user is logged in show saved books and logout */}
+            {Auth.loggedIn() ? (
+              <>
+                <Nav.Link as={Link} to='/planet/:planet.name' element={<Planet planet={planet}/>}>
+                  Planet
+                </Nav.Link>
+                <Nav.Link onClick={Auth.logout}>Logout</Nav.Link>
+              </>
+            ) : (
+              <Nav.Link onClick={() => setShowModal(true)}>Login/Sign Up</Nav.Link>
+            )}
           </Nav>
         </Navbar.Collapse>
       </Container>
-    </Navbar>
+    </Navbar> *
+    {/* set modal data up */}
+    <Modal
+      size='lg'
+      show={showModal}
+      onHide={() => setShowModal(false)}
+      aria-labelledby='signup-modal'>
+      {/* tab container to do either signup or login component */}
+      <Tab.Container defaultActiveKey='login'>
+        <Modal.Header closeButton>
+          <Modal.Title id='signup-modal'>
+            <Nav variant='pills'>
+              <Nav.Item>
+                <Nav.Link eventKey='login'>Login</Nav.Link>
+              </Nav.Item>
+              <Nav.Item>
+                <Nav.Link eventKey='signup'>Sign Up</Nav.Link>
+              </Nav.Item>
+            </Nav>
+          </Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Tab.Content>
+            <Tab.Pane eventKey='login'>
+              <LoginForm handleModalClose={() => setShowModal(false)} />
+            </Tab.Pane>
+            <Tab.Pane eventKey='signup'>
+              <SignUpForm handleModalClose={() => setShowModal(false)} />
+            </Tab.Pane>
+          </Tab.Content>
+        </Modal.Body>
+      </Tab.Container>
+    </Modal>
+  </>
   );
-}
-
-export default BasicExample;
-
-// ! MERN HW NAVBAR
-
-{/* <> */}
-{/* COMMENTED OUT */}
-      {/* <Navbar bg='dark' variant='dark' expand='lg'>
-        <Container fluid>
-          <Navbar.Brand as={Link} to='/'>
-            Google Books Search
-          </Navbar.Brand>
-          <Navbar.Toggle aria-controls='navbar' />
-          <Navbar.Collapse id='navbar'>
-            <Nav className='ml-auto'>
-              <Nav.Link as={Link} to='/'>
-                Search For Books
-              </Nav.Link> */}
-              
-              {/* if user is logged in show saved books and logout */}
-
-              {/* COMMENTED OUT */}
-              {/* {Auth.loggedIn() ? (
-                <>
-                  <Nav.Link as={Link} to='/saved'>
-                    See Your Books
-                  </Nav.Link>
-                  <Nav.Link onClick={Auth.logout}>Logout</Nav.Link>
-                </>
-              ) : (
-                <Nav.Link onClick={() => setShowModal(true)}>Login/Sign Up</Nav.Link>
-              )} */}
-
-            {/* </Nav>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar> */}
-
-      {/* set modal data up */}
-
-      {/* <Modal
-        size='lg'
-        show={showModal}
-        onHide={() => setShowModal(false)}
-        aria-labelledby='signup-modal'> */}
-
-        {/* tab container to do either signup or login component */}
-
-        {/* <Tab.Container defaultActiveKey='login'>
-          <Modal.Header closeButton>
-            <Modal.Title id='signup-modal'>
-              <Nav variant='pills'>
-                <Nav.Item>
-                  <Nav.Link eventKey='login'>Login</Nav.Link>
-                </Nav.Item>
-                <Nav.Item>
-                  <Nav.Link eventKey='signup'>Sign Up</Nav.Link>
-                </Nav.Item>
-              </Nav>
-            </Modal.Title>
-          </Modal.Header>
-          <Modal.Body>
-            <Tab.Content>
-              <Tab.Pane eventKey='login'>
-                <LoginForm handleModalClose={() => setShowModal(false)} />
-              </Tab.Pane>
-              <Tab.Pane eventKey='signup'>
-                <SignUpForm handleModalClose={() => setShowModal(false)} />
-              </Tab.Pane>
-            </Tab.Content>
-          </Modal.Body>
-        </Tab.Container>
-      </Modal>
-    </> */}
+};
