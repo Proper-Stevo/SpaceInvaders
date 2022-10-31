16829a67d7419aaa31fc95d9a954a60ff2edc28f
Martha A <mladame43@gmail.com>
2022-10-30 20:07:54 -0700

    added initial css for planet name card, planet saved comment section

--- client/src/pages/Planet/PlanetComment.js
+++ client/src/pages/Planet/PlanetComment.js
@@ -84,7 +84,7 @@ export default function PlanetComment(planet) {
     ) : (
         <div className='userCommentCard'>
             <h2 className='notes-title'>SATURN NOTES</h2>
-            <Card>
+            <Card className='comment-card-text'>
                 <Card.Body>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</Card.Body>
             </Card>
             {/* <Button variant="outline-primary">Update Notes</Button>{' '} */}
