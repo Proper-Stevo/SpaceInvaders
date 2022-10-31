bf366438863e2fb7a981a67f1901ece6a60c9473
Martha A <mladame43@gmail.com>
2022-10-30 22:02:08 -0700

    planet facts css in progress, debug row/col in progress

--- client/src/pages/Planet/PlanetFacts.js
+++ client/src/pages/Planet/PlanetFacts.js
@@ -10,86 +10,86 @@ import Col from 'react-bootstrap/Col';
 export default function Planet(planet) {
 
 
-    
+
     return (
         <Container className='planetFactsContainer'>
-            <Row className='planet-facts-row'>
-            <Col>
-            <Card className='planet-fact-card'>
-                <Card.Body>
-                    <Card.Subtitle className="mb-2 text-muted">Mass</Card.Subtitle>
-                    <Card.Title>5.68336 units</Card.Title>
-                    {/* <Card.Title>{planet.mass} units</Card.Title> */}
-                </Card.Body>
-            </Card>
+            <Col className='planet-facts-row'>
+                <Row>
+                    <Card className='planet-fact-card'>
+                        <Card.Body>
+                            <Card.Subtitle className="mb-2 text-muted">Mass</Card.Subtitle>
+                            <Card.Title>5.68336 units</Card.Title>
+                            {/* <Card.Title>{planet.mass} units</Card.Title> */}
+                        </Card.Body>
+                    </Card>
+                </Row>
+                <Row>
+                    <Card className='planet-fact-card'>
+                        <Card.Body>
+                            <Card.Subtitle className="mb-2 text-muted">Gravity</Card.Subtitle>
+                            <Card.Title>10.44 units</Card.Title>
+                            {/* <Card.Title>{planet.gravity} units</Card.Title> */}
+                        </Card.Body>
+                    </Card>
+                </Row>
+                <Row>
+                    <Card className='planet-fact-card'>
+                        <Card.Body>
+                            <Card.Subtitle className="mb-2 text-muted">Density</Card.Subtitle>
+                            <Card.Title>0.6871 units</Card.Title>
+                            {/* <Card.Title>{planet.density} units</Card.Title> */}
+                        </Card.Body>
+                    </Card>
+                </Row>
+                <Row>
+                    <Card className='planet-fact-card'>
+                        <Card.Body>
+                            <Card.Subtitle className="mb-2 text-muted">Average Temp.</Card.Subtitle>
+                            <Card.Title>134 units</Card.Title>
+                            {/* <Card.Title>{planet.avgTemp} units</Card.Title> */}
+                        </Card.Body>
+                    </Card>
+                </Row>
             </Col>
-            <Col>
-            <Card className='planet-fact-card'>
-                <Card.Body>
-                    <Card.Subtitle className="mb-2 text-muted">Gravity</Card.Subtitle>
-                    <Card.Title>10.44 units</Card.Title>
-                    {/* <Card.Title>{planet.gravity} units</Card.Title> */}
-                </Card.Body>
-            </Card>
+            <Col className='planet-facts-row'>
+                <Row>
+                    <Card className='planet-fact-card'>
+                        <Card.Body>
+                            <Card.Subtitle className="mb-2 text-muted">Sideral Orbit</Card.Subtitle>
+                            <Card.Title>10759.22 units</Card.Title>
+                            {/* <Card.Title>{planet.sideralOrbit} units</Card.Title> */}
+                        </Card.Body>
+                    </Card>
+                </Row>
+                <Row>
+                    <Card className='planet-fact-card'>
+                        <Card.Body>
+                            <Card.Subtitle className="mb-2 text-muted">Sideral Rotation</Card.Subtitle>
+                            <Card.Title>10.656 units</Card.Title>
+                            {/* <Card.Title>{planet.sideralRotation} units</Card.Title> */}
+                        </Card.Body>
+                    </Card>
+                </Row>
+                <Row>
+                    <Card className='planet-fact-card'>
+                        <Card.Body>
+                            <Card.Subtitle className="mb-2 text-muted">Distance from Sun</Card.Subtitle>
+                            <Card.Title>889000000 units</Card.Title>
+                            {/* <Card.Title>{planet.distanceFromSun} units</Card.Title> */}
+                        </Card.Body>
+                    </Card>
+                </Row>
+                <Row>
+                    <Card className='planet-fact-card'>
+                        <Card.Body>
+                            <Card.Subtitle className="mb-2 text-muted">Year Length</Card.Subtitle>
+                            <Card.Title>10585 units</Card.Title>
+                            {/* <Card.Title>{planet.yearLength} units</Card.Title> */}
+                        </Card.Body>
+                    </Card>
+                </Row>
             </Col>
-            <Col>
-            <Card className='planet-fact-card'>
-                <Card.Body>
-                    <Card.Subtitle className="mb-2 text-muted">Density</Card.Subtitle>
-                    <Card.Title>0.6871 units</Card.Title>
-                    {/* <Card.Title>{planet.density} units</Card.Title> */}
-                </Card.Body>
-            </Card>
-            </Col>
-            <Col>
-            <Card className='planet-fact-card'>
-                <Card.Body>
-                    <Card.Subtitle className="mb-2 text-muted">Average Temp.</Card.Subtitle>
-                    <Card.Title>134 units</Card.Title>
-                    {/* <Card.Title>{planet.avgTemp} units</Card.Title> */}
-                </Card.Body>
-            </Card>
-            </Col>
-            </Row>
-            <Row className='planet-facts-row'>
-            <Col>
-            <Card className='planet-fact-card'>
-                <Card.Body>
-                    <Card.Subtitle className="mb-2 text-muted">Sideral Orbit</Card.Subtitle>
-                    <Card.Title>10759.22 units</Card.Title>
-                    {/* <Card.Title>{planet.sideralOrbit} units</Card.Title> */}
-                </Card.Body>
-            </Card>
-            </Col>
-            <Col>
-            <Card className='planet-fact-card'>
-                <Card.Body>
-                    <Card.Subtitle className="mb-2 text-muted">Sideral Rotation</Card.Subtitle>
-                    <Card.Title>10.656 units</Card.Title>
-                    {/* <Card.Title>{planet.sideralRotation} units</Card.Title> */}
-                </Card.Body>
-            </Card>
-            </Col>
-            <Col>
-            <Card className='planet-fact-card'>
-                <Card.Body>
-                    <Card.Subtitle className="mb-2 text-muted">Distance from Sun</Card.Subtitle>
-                    <Card.Title>889000000 units</Card.Title>
-                    {/* <Card.Title>{planet.distanceFromSun} units</Card.Title> */}
-                </Card.Body>
-            </Card>
-            </Col>
-            <Col>
-            <Card className='planet-fact-card'>
-                <Card.Body>
-                    <Card.Subtitle className="mb-2 text-muted">Year Length</Card.Subtitle>
-                    <Card.Title>10585 units</Card.Title>
-                    {/* <Card.Title>{planet.yearLength} units</Card.Title> */}
-                </Card.Body>
-            </Card>
-            </Col>
-            </Row>
         </Container>
-        
+
     )
 };
